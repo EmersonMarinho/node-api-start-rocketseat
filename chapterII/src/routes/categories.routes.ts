87be4efd34dc98fb2240facbd512dff8cd766be9
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-09-25 16:18:02 -0300

    feat: added model and TS configuration

---
+++ chapterII/src/routes/categories.routes.ts
@@ -0,0 +1,21 @@
+import { Router, Request, Response } from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const category = new Category();
+
+  Object.assign(category, { name, description, created_at: new Date() });
+
+  categories.push(category);
+
+  return response.status(201).json({ categories });
+});
+
+export { categoriesRoutes };
