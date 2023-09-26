96afeace6d7af4840e82cd6af3a4bb26706df13a
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-09-26 18:05:32 -0300

    feat: added useCases and created controller to be more scalable

--- chapterII/src/routes/categories.routes.ts
+++ chapterII/src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router, Request, Response } from "express";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createCategorySerivce = new CreateCategoryService(categoriesRepository);
-
-  createCategorySerivce.execute({ name, description });
-
-  response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (_request: Request, response: Response) => {
