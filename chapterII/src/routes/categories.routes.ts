c46bed80a74601b53c9d0d23cab2f70b474baefd
EmersonMarinho <Emerson_marinhoaz@hotmail.com>
2023-10-02 15:09:34 -0300

    refactor: refactor category to add it to the Database and list it from the database

--- chapterII/src/routes/categories.routes.ts
+++ chapterII/src/routes/categories.routes.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from "express";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import multer from "multer";
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import createCategoryController from "../modules/cars/useCases/createCategory";
+import listCategoriesController from "../modules/cars/useCases/listCategories";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
@@ -12,12 +12,11 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
-  console.log("Reload funcionando");
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (_request: Request, response: Response) => {
-  return listCategoriesController.handle(_request, response);
+  return listCategoriesController().handle(_request, response);
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
